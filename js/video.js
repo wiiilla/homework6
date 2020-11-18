0ae891093ea88b53bd263bce3e237422c3d9a21a
Willa Hua <bhua@umich.edu>
2020-11-18 00:46:19 -0500

    initial try

--- js/video.js
+++ js/video.js
@@ -5,10 +5,55 @@ window.addEventListener("load", function() {
 });
 
 document.querySelector("#play").addEventListener("click", function() {
+	video.play();
 	console.log("Play Video");
-});
+	let vol = document.querySelector("#volume").innerHTML = video.volume * 100 + '%';
+}); 
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	video.pause();
+	console.log("Pause Video");
 });
 
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate /= 0.9;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 5 > video.duration) {
+		video.currentTime = 0 ;
+	} else {
+		video.currentTime += 5;
+	}
+	console.log("Current location " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == true) {
+		video.muted = false;
+		this.innerHTML = 'Mute';
+	} else {
+		video.muted = true;
+		this.innerHTML = 'Unmute';
+	}
+});
+
+document.querySelector("#volumeSlider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+	let vol = document.querySelector("#volume").innerHTML = this.value + '%';
+	console.log(video.volume);
+});
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.classList.add('oldTime')
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	video.classList.remove('oldTime')
+});
